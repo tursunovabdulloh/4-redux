fd44726cf8a1890f712de44ba2639216f96814ad
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-06-02 21:18:51 +0300

    1

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 function MainLayout() {
